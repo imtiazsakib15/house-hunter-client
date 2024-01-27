0d6e677978aa2f2845ad37949b0289b3f84df419
Imtiaz Ahmed Sakib <sakib.imtiaz15@gmail.com>
2024-01-27 23:31:05 +0600

    Send token to server

--- src/hooks/useUserDetails.jsx
+++ src/hooks/useUserDetails.jsx
@@ -4,19 +4,19 @@ const useUserDetails = () => {
   const axiosPublic = useAxiosPublic();
   const accessToken = localStorage.getItem(accessToken);
   console.log(accessToken);
-  //   axiosPublic
-  //     .post("/register", data)
-  //     .then((res) => {
-  //       if (res.status === 201) {
-  //         toast.success("Successfully registered!");
-  //         navigate("/");
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //       if (error?.response?.data?.error)
-  //         toast.error(error?.response?.data?.error);
-  // });
+    axiosPublic
+      .post("/register", data)
+      .then((res) => {
+        if (res.status === 201) {
+         toast.success("Successfully registered!");
+          navigate("/");
+       }
+     })
+      .catch((error) => {
+       console.log(error);
+        if (error?.response?.data?.error)
+          toast.error(error?.response?.data?.error);
+  });
   return <div></div>;
 };
 
